004046fdcfd63c729e2ef86cf29f0931cb6f6900
Sumesh <sumesh.majhi.5@gmail.com>
2021-11-23 17:27:48 +0530

    📦 NEW: Add Theme Switcher

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
-import React from "react"
+import React, { useContext } from "react"
 import Layout from "../components/Layout"
+import ModeButton from "../components/ModeButton"
+import { ModeContext } from "../context/ModeProvider"
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useContext(ModeContext)
+
   return (
     <Layout>
       <div>Hello world!</div>
+      <ModeButton darkMode={darkMode} setDarkMode={setDarkMode} />
     </Layout>
   )
 }
