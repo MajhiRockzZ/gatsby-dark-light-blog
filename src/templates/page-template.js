6f47f0410177c9d356e13907fbe00dfc1ff1689f
Sumesh <sumesh.majhi.5@gmail.com>
2021-11-24 09:39:14 +0530

    📦 NEW: Add Basic Template Setup

---
+++ src/templates/page-template.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import Seo from "../components/Seo"
+
+const Page = () => {
+  return (
+    <Layout>
+      <Seo title="about" />
+      <main>
+        <h1>About Page</h1>
+      </main>
+    </Layout>
+  )
+}
+
+export default Page
